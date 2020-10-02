7678e5cda15493d79a9c43d2c49502b7dfb2fd03
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-10-02 13:45:01 -0500

    ex2 works after fixing syntax and spelling errors

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,6 +5,9 @@ class Ticket:
         self.destination = destination
 
 
+cache = {}  # empty dictionary to recheck values already added
+
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
@@ -12,8 +15,25 @@ def reconstruct_trip(tickets, length):
      1st ticket => source == None  destination of 1st ticket == 2nd tickets source
      The next tickets source == previous tickets destination
      Final ticket destination == None
-    
+     If the destination of last ticket and source to next ticket match, the next ticket should be listed after the destination ticket
+     should iterate over list checking current destination to next.source
+     if source == None ... prepend to list (make sure it is first)
+     if destination == None ... append to end of list, len(self -1) make sure it is last
+     if current.destination == next.source
+     next.source. append to list
     """
     # Your code here
+
+    route = [None] * length  # make an empty route list to match the length
+
+    for t in tickets:  # for each ticket in the tickets
+        # add source:destination of each to cache
+        cache[t.source] = t.destination
+    # make next location destination to starting location for next ticket
+    next = cache["NONE"]  # make sure all caps otherwise does not work
+    for i in range(0, length):  # start at index 0 and go thru length
+        route[i] = next
+    # make next value be current place, next place to go in cache
+        next = cache[next]
 
     return route
